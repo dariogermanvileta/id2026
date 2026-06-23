bcf27ce482df1c57f308ecde3eaf35ee81826237
dariogermanvileta <dariogermanvileta@gmail.com>
2026-06-23 14:06:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app = FastAPI(title="I+D 2026 API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
